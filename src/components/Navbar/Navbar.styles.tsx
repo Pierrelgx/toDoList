a8bb7e00cadfc2ad510e1da52f64baf06251d3b9
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-08-12 08:42:40 +0200

    little update test

--- src/components/Navbar/Navbar.styles.tsx
+++ src/components/Navbar/Navbar.styles.tsx
@@ -2,5 +2,18 @@ import styled from "styled-components";
 
 export const Background = styled.div`
   background-color: #323232;
+`;
 
+export const Nav = styled.nav`
+  background-color: #323232;
+  height: 50px;
+`;
+
+export const List = styled.li`
+  background-color: #323232;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
 `;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,15 @@
-import { Background } from "./Navbar.styles";
+import { Nav } from "./Navbar.styles";
 
 export const Navbar = (): JSX.Element => {
     return(
-        <>
-        <Background> 
-        </Background>
-        </>
+        <Nav>
+            <ul className="list">
+                <li className="items">Choix 1</li>
+                <li className="items">Choix 2</li>
+                <li className="items">Choix 3</li>
+            </ul>
+            <button className="btn">BTN</button>
+        </Nav>
       )
     
   };
