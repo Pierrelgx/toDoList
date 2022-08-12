cae7806b2e3c75e70818f756b3407482424297da
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-08-12 08:27:20 +0200

    add files to navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Background } from "./Navbar.styles";
+
+export const Navbar = (): JSX.Element => {
+    return(
+        <>
+        <Background> 
+        </Background>
+        </>
+      )
+    
+  };
