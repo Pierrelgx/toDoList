d07d9bbb807700ff92a51e30c819c57b055ea7c6
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-08-12 16:41:16 +0200

    Remove example code

--- src/components/Todo/Todo.styles.tsx
+++ src/components/Todo/Todo.styles.tsx
@@ -12,32 +12,3 @@ export const TodoTitle = styled.h1`
   margin: 0;
   color: black;
 `;
-
-export const BoxContainerToBeRemoved = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 16px;
-`;
-
-export const BoxToBeRemoved = styled.div<{
-  color: "gray" | "white" | "black" | "blue"
-}>`
-  width: 60px;
-  height: 60px;
-  background-color: ${(props ) => {
-    switch (props.color) {
-      case "gray": {
-        return props.theme.colors.spanishGray
-      }
-      case "blue": {
-        return props.theme.colors.royalBlue
-      }
-      case "black": {
-        return props.theme.colors.eerieBlack
-      }
-      case "white": {
-        return props.theme.colors.cultured
-      }
-    }
-  }};
-`;
--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,15 +1,9 @@
-import { BoxContainerToBeRemoved, BoxToBeRemoved, TodoTitle, TodoWrapper } from "./Todo.styles";
+import { TodoTitle, TodoWrapper } from "./Todo.styles";
 
 export const Todo = (): JSX.Element => {
   return (
     <TodoWrapper>
       <TodoTitle>Coucou test Piou Piou</TodoTitle>
-      <BoxContainerToBeRemoved>
-        <BoxToBeRemoved color="black" />
-        <BoxToBeRemoved color="blue" />
-        <BoxToBeRemoved color="gray" />
-        <BoxToBeRemoved color="white" />
-      </BoxContainerToBeRemoved>
     </TodoWrapper>
   )
 };
