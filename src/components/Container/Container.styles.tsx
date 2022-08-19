cd28de534504067c5ff5fa656b4435994c24b2f7
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-08-19 23:07:01 +0200

    update Upsert

--- src/components/Container/Container.styles.tsx
+++ src/components/Container/Container.styles.tsx
@@ -4,10 +4,11 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: flex-start;
   gap: 16px;
   padding: 0 16px;
+  height: 100%;
   width: 100%;
+  background-color: #16181d ;
 
   /* Desktop */
   @media (min-width: 768px) {
