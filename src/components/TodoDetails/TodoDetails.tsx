412ed73e6a5c58488c76c522eeca331cf5d473a6
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-08-22 16:50:45 +0200

    remove things usefull and fix indentation

--- src/components/TodoDetails/TodoDetails.tsx
+++ src/components/TodoDetails/TodoDetails.tsx
@@ -1,16 +1,6 @@
-// import { Todo } from "components/Todo";
-// import { TodoDescription } from "components/Todo/Todo.styles";
 import { TodoDetailsRow, TodoDescription, TodoTask, TodoTitle } from "components/TodoDetails/TodoDetails.styles";
 import { Container } from "components/Container";
 
-
-
-/**
- * 1. Create details page as seen in app from Florian (title with label, description with label in a 2 column grid)
- * 2. Labels to the left of the grid and values to the right (initially with placeholder data)
- * 3. Add actual data from a todo through props (see Todo component)
- * 
- */
 
 export const TodoDetails = (): JSX.Element => {
 
--- src/components/TodoUpsert/TodoUpsert.tsx
+++ src/components/TodoUpsert/TodoUpsert.tsx
@@ -2,13 +2,11 @@ import { Container } from "components/Container";
 import { Input, TodoUpsertLabel, TodoForm } from "./TodoUpsert.styles";
 
 /**
- * 1. Create edit page with an accessible form (with the correct semantic elements)
- * 2. Create form validation (KISS - Keep it simple stupid)
- * 3. On submit console.log("submitted", { name, description, date }) and show any errors for now (red text under field + red border if field is in error)
+ * On submit console.log("submitted", { name, description, date }) and show any errors for now (red text under field + red border if field is in error)
  */
 export const TodoUpsert = (): JSX.Element => {
   return (
-    <>
+  
       <Container>
         <TodoForm>
         
@@ -30,6 +28,6 @@ export const TodoUpsert = (): JSX.Element => {
         
         </TodoForm>
       </Container>
-    </>
+    
   )
 }
